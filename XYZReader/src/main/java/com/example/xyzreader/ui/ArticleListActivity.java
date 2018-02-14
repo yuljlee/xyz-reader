91a28270fb0c6db7f4f76ee0fb3c0459d3f2c01e
Yul Jae Lee <yuljlee9@gmail.com>
2018-02-14 00:40:09 -0800

    Update style

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -8,6 +8,7 @@ import android.content.IntentFilter;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.RecyclerView;
@@ -120,6 +121,10 @@ public class ArticleListActivity extends ActionBarActivity implements
         StaggeredGridLayoutManager sglm =
                 new StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(sglm);
+
+        // add snackbar
+        Snackbar.make(mRecyclerView, R.string.snackbar_text, Snackbar.LENGTH_LONG)
+                .show();
     }
 
     @Override
