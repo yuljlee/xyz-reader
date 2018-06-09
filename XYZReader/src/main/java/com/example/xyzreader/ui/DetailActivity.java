fcf5c37b88cad800a8dbbb3f542afe9d4d446c29
Yul Jae Lee <yuljlee9@gmail.com>
2018-06-08 19:16:45 -0700

    feat: Add a DetailActivity to test Material Design

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.xyzreader.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.xyzreader.R;
+
+public class DetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+}
